4fbdc2135456c736e44f4a5a029d30b154c8b4ff
Valjean Clark <valjeanbud@gmail.com>
2015-03-23 20:59:48 -0700

    changed to joel server

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -2,13 +2,13 @@
 
 angular.module('ewbClient')
 .service('SocketService', function(socketFactory) {
-	
+
 	var socket = socketFactory({
-		ioSocket: io.connect('localhost:3000')
- 		// ioSocket: io.connect('http://ewb-server.paas1.icloud.intel.com:80')
+		// ioSocket: io.connect('localhost:3000')
+ 		ioSocket: io.connect('http://joelmax.com:9000')
 	});
 	socket.forward('error');
-	
+
 	return socket;
 
 });
